d5c756b0ce31867e5aba82cdea718334ca76b8d9
Robert <robert@tooconnect.nl>
2016-09-18 10:58:36 +0200

    Initialize repo

---
+++ server.js
@@ -0,0 +1,43 @@
+/**
+ * Created by Robert on 18-9-2016.
+ */
+
+
+var express = require("express");
+var app = express();
+const PORT = 3000;
+
+var middleware={
+    requireAuthentication: function(req,res,next){
+        console.log("private route hit");
+        next();
+    },
+    logger:function(req,res,next){
+        new Date().toString();
+      console.log(("Request: " + new Date().toString() + " "+ req.method + " " + req.originalUrl));
+
+        next();
+    }
+}
+
+app.use(middleware.logger);
+//app.use(middleware.requireAuthentication)
+/*
+
+app.get("/",function(req,res){
+    res.send("hello express");
+});
+
+ */
+app.get('/about',middleware.requireAuthentication,function (req,res) {
+   res.send("<h1>About us</h1>")
+});
+
+app.use(express.static(__dirname + '/public'));
+console.log(__dirname);
+
+
+app.listen(PORT,function(){
+    console.log("Express server started on port: " + PORT);
+});
+
